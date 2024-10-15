2dd0c162e1f2d72485e209044cc2007c5adf62fe
Luc Langevin <luclange20@gmail.com>
2024-10-15 14:26:53 -0400

    changes in nomenclature

--- Ashton_succurcales.R
+++ Ashton_succurcales.R
@@ -1,6 +1,8 @@
-Sys.getlocale()
+#Reading 'https://ashtonrestaurants.ca/succursales' to get geolocalization of Ashton restos
+
+#Sys.getlocale()
 Sys.setlocale("LC_ALL", 'en_US.UTF-8')
-Sys.getenv()
+#Sys.getenv()
 Sys.setenv(LANG = "en")
 #sessionInfo()
 
@@ -21,29 +23,31 @@ library(tidyverse)
 library(rvest)
 
 #read https://ashtonrestaurants.ca/succursales
-#a_page <- read_html('https://ashtonrestaurants.ca/succursales')
-a_page <- read_html('view-source_https___ashtonrestaurants.ca_succursales.html')
+#a_s_page: Ashton Succurcales Page
+a_s_page <- read_html('https://ashtonrestaurants.ca/succursales')
+#Local copy
+#a_s_page <- read_html('view-source_https___ashtonrestaurants.ca_succursales.html')
 
 #Nom Restaurant
 #.h4-like
-a_page %>% html_nodes(".h4-like") %>% html_text()
-a_page %>% html_nodes(".h4-like") %>% html_text() %>% .[2]
-Resto_Name <- a_page %>% html_nodes(".h4-like")
+a_s_page %>% html_nodes(".h4-like") %>% html_text()
+a_s_page %>% html_nodes(".h4-like") %>% html_text() %>% .[2]
+Resto_Name <- a_s_page %>% html_nodes(".h4-like")
 
 #ID resto
 #.js-restaurants-map-restaurant
-a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_text()
-a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_text() %>% .[2]
-a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-key")
-a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-name")
-a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lat")
-a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lng")
-resto_ID <- a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-key")
-resto_name <- a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-name")
-resto_lat <- a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lat")
-resto_lng <- a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lng")
-resto_lat <- a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lat") %>% gsub(pattern=",", replacement=".") %>% as.numeric
-resto_lng <- a_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lng") %>% gsub(pattern=",", replacement=".") %>% as.numeric
+a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_text()
+a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_text() %>% .[2]
+a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-key")
+a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-name")
+a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lat")
+a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lng")
+resto_ID <- a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-key")
+resto_name <- a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-name")
+#resto_lat <- a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lat")
+#resto_lng <- a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lng")
+resto_lat <- a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lat") %>% gsub(pattern=",", replacement=".") %>% as.numeric
+resto_lng <- a_s_page %>% html_nodes(".js-restaurants-map-restaurant") %>% html_attr("data-lng") %>% gsub(pattern=",", replacement=".") %>% as.numeric
 #as.numeric(gsub(",",".", resto_lat))
 
 #Build dataset
@@ -52,13 +56,13 @@ resto_info <- data.frame(resto_ID, resto_name, resto_lat, resto_lng, stringsAsFa
 
 #Adresse telephone horaire
 #.restaurant
-a_page %>% html_nodes(".restaurant") %>% html_text()
-a_page %>% html_nodes(".restaurant") %>% html_text() %>% .[1]
+a_s_page %>% html_nodes(".restaurant") %>% html_text()
+a_s_page %>% html_nodes(".restaurant") %>% html_text() %>% .[1]
 
 #Adresse telephone
 #.rte--no-m
-a_page %>% html_nodes(".rte--no-m") %>% html_text()
-a_page %>% html_nodes(".rte--no-m") %>% html_text() %>% .[1]
+a_s_page %>% html_nodes(".rte--no-m") %>% html_text()
+a_s_page %>% html_nodes(".rte--no-m") %>% html_text() %>% .[1]
 
 #Mapping the resto
 # some standard map packages.
